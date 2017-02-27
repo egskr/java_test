8a14d6687ca49099b64df5e636f7d0989fb81a19
root <root@epbyminw2033t1.minsk.epam.com>
2017-02-27 15:46:56 +0300

    chang 2

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -1,5 +1,41 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+int i = 1;
+        while (i < 5) {
+            System.out.print(i + " ");
+            i++;
+        }
+int i = 1;
+        while (i < 5) {
+            System.out.print(i + " ");
+            i++;
+        }
+int i = 1;
+        while (i < 5) {
+            System.out.print(i + " ");
+            i++;
+        }
+int i = 1;
+        while (i < 5) {
+            System.out.print(i + " ");
+            i++;
+        }
+int i = 1;
+        while (i < 5) {
+            System.out.print(i + " ");
+            i++;
+        }
+int i = 1;
+        while (i < 5) {
+            System.out.print(i + " ");
+            i++;
+        }
+int i = 1;
+        while (i < 5) {
+            System.out.print(i + " ");
+            i++;
+        }
+
      }
 } 
