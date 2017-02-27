8e16ad5a1758b6dac6ec42b4fc137788d9ccde03
root <root@epbyminw2033t1.minsk.epam.com>
2017-02-27 15:49:44 +0300

    chang 3

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -6,32 +6,32 @@ int i = 1;
             System.out.print(i + " ");
             i++;
         }
-int i = 1;
+int i1 = 1;
+        while (i1 < 5) {
+            System.out.print(i1 + " ");
+            i1++;
+        }
+int i2 = 1;
+        while (i2 < 5) {
+            System.out.print(i + " ");
+            i++;
+        }
+int i3 = 1;
         while (i < 5) {
             System.out.print(i + " ");
             i++;
         }
-int i = 1;
+int i4 = 1;
         while (i < 5) {
             System.out.print(i + " ");
             i++;
         }
-int i = 1;
+int i5 = 1;
         while (i < 5) {
             System.out.print(i + " ");
             i++;
         }
-int i = 1;
-        while (i < 5) {
-            System.out.print(i + " ");
-            i++;
-        }
-int i = 1;
-        while (i < 5) {
-            System.out.print(i + " ");
-            i++;
-        }
-int i = 1;
+int i6 = 1;
         while (i < 5) {
             System.out.print(i + " ");
             i++;
